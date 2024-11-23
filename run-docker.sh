2484dc67afe7b90d94a41d41e1e9a887597a0d52
Vibhumkaila <100128312+Vibhumkaila@users.noreply.github.com>
2024-11-23 17:33:11 +0530

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -3,14 +3,14 @@
 # Run Client (Frontend) Docker container
 echo "Building and running the Client container..."
 cd client
-docker build -t my-app-client .
-docker run -d -p 80:80 --name my-app-client my-app-client
+docker build -t client .
+docker run -d -p 80:80 --name client client 
 
 # Run Server (Backend) Docker container
 echo "Building and running the Server container..."
 cd ../server
-docker build -t my-app-server .
-docker run -d -p 5000:3000 --name my-app-server my-app-server
+docker build -t server .
+docker run -d -p 5000:3000 --name server server
 
 echo "Containers are running:"
 echo "Client is available on http://localhost"
